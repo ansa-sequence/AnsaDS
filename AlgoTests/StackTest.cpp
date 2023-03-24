e64dc834ea2ae1860f43c751ec8ad0d018cf0f18
ansa.sequence <ansa.sequence@gmail.com>
2023-03-24 11:00:44 +0100

    In this CL I did : - Removed useless comments

--- AlgoTests/StackTest.cpp
+++ AlgoTests/StackTest.cpp
@@ -5,18 +5,14 @@ namespace stackTests
 {
 	using namespace ansa;
 
-	// Test fixture for the Stack class
 	class StackTest : public testing::Test
 	{
 	protected:
-		// Helper function to create a stack with the given elements
 		Stack<int> createStack(const std::initializer_list<int>& elements) const { return Stack<int>(elements); }
 	};
 
-	// Tests the push and pop methods of the Stack class
 	TEST_F(StackTest, PushPopTest)
 	{
-		// Arrange
 		auto s = createStack({1, 2, 3});
 
 		s.push(4);
@@ -36,13 +32,10 @@ namespace stackTests
 		EXPECT_THROW(s.top(), std::length_error);
 	}
 
-	// Tests the top method of the Stack class
 	TEST_F(StackTest, TopTest)
 	{
-		// Arrange
 		auto stack = createStack({1, 2, 3});
 
-		// Act and Assert
 		ASSERT_EQ(stack.top(), 3);
 
 		stack.pop();
@@ -51,18 +44,14 @@ namespace stackTests
 		stack.pop();
 		ASSERT_EQ(stack.top(), 1);
 
-		// Act and Assert
 		stack.pop();
 		EXPECT_THROW(stack.pop(), std::length_error);
 	}
 
-	// Tests the empty method of the Stack class
 	TEST_F(StackTest, EmptyTest)
 	{
-		// Arrange
 		Stack<int> s({});
 
-		// Act and Assert
 		ASSERT_TRUE(s.empty());
 
 		s.push(1);
@@ -72,13 +61,10 @@ namespace stackTests
 		ASSERT_TRUE(s.empty());
 	}
 
-	// Tests the size method of the Stack class
 	TEST_F(StackTest, SizeTest)
 	{
-		// Arrange
 		auto s = createStack({1, 2, 3});
 
-		// Act and Assert
 		ASSERT_EQ(s.size(), 3);
 
 		s.push(4);
@@ -95,7 +81,6 @@ namespace stackTests
 
 		s.pop();
 
-		// Assert and Act
 		EXPECT_THROW(s.pop(), std::length_error);
 	}
 }
